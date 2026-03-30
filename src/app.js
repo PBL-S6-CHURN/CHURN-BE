f364cc0a84fddef7ad1b13163463d17c725f10ae
max.desanta <maxdesanta1@gmail.com>
2026-03-30 20:02:02 +0700

    first project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+// router
+// const { router } = require("./router");
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
